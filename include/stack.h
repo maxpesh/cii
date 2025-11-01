4bcb53ff4745ec64227094dce2422dd2e73a6541
Max Peshkov <maxpeshkovit@gmail.com>
2025-11-01 16:48:58 +0400

    Move #include inside "include guard"

--- include/stack.h
+++ include/stack.h
@@ -1,13 +1,13 @@
-#include <stdbool.h>
-
 #ifndef STACK_INCLUDED
 #define STACK_INCLUDED
+
+#include <stdbool.h>
 
 #define T Stack_T
 typedef struct T *T;
 
 extern T     Stack_new  (void);
-extern bool   Stack_empty(T stk);
+extern bool  Stack_empty(T stk);
 extern void  Stack_push (T stk, void *x);
 extern void *Stack_pop  (T stk);
 extern void  Stack_free (T *stk);
